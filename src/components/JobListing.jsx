bd32bf5a5e58f5e19ddb6c4f78ab17d207255c9b
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-18 18:48:24 -0700

    Time Tag
    Updated the `datetime` attribute for the time tag to `dateTime` to fix
    the lint error.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -2,7 +2,7 @@ const JobListing = () => {
     return (
         <div className="job-info">
             <p>
-                <time datetime="2015-08-01">August 2014</time> - <time datetime="2022-03-26">March 2022</time>
+                <time dateTime="2015-08-01">August 2014</time> - <time dateTime="2022-03-26">March 2022</time>
             </p>
             <h3>Senior Web Developer &middot; Stamps.com</h3>
             <p>Delivered high-quality, robust production code for a diverse array of projects.</p>
