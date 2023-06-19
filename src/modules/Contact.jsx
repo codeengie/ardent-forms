f31fac286f921a581fea519e2586329749b6c7a1
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-18 18:49:21 -0700

    Contact Page Fetch
    Created a test function to fetch some data while I work on the backend
    in AWS.

--- src/modules/Contact.jsx
+++ src/modules/Contact.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+
 const Contact = () => {
+    const [movie, setMovie] = useState([]);
+
     // Functions need to be declared before return()
-    const sendForm = () => {
+    const sendForm = async () => {
         console.log('Sent an email!');
+
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+
+            if (!response.ok) {
+                new Error(`Request failed with status ${response.status}`);
+            }
+
+            const data = await response.json();
+            setMovie(data);
+            console.log(data);
+        } catch (error) {
+            console.error('Error', error.message);
+        }
     };
 
     return (
         <div className="contact">
             <button onClick={sendForm}>Send</button>
+            <p>{movie.count}</p>
         </div>
     )
 }
