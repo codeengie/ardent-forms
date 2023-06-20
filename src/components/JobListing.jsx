3080212b047c5c3d7cf41a69c9dce77a9b9d702b
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 11:53:01 -0700

    Job And Tech Listings
    Began fleshing out the JobListing component along with creating styles
    and linking them.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,11 +1,20 @@
-const JobListing = () => {
+import './JobListing.scss';
+import TechListing from "./TechListing.jsx";
+
+const JobListing = (props) => {
     return (
-        <div className="job-info">
+        // @todo Look into this linting error: "Missing in props validation"
+        <div className={`job-info ${props.cName}`}>
             <p>
                 <time dateTime="2015-08-01">August 2014</time> - <time dateTime="2022-03-26">March 2022</time>
             </p>
-            <h3>Senior Web Developer &middot; Stamps.com</h3>
-            <p>Delivered high-quality, robust production code for a diverse array of projects.</p>
+            <h3 className="job-info__title">Senior Web Developer &middot; Stamps.com</h3>
+            <p className="job-info__text">Delivered high-quality, robust production code for a diverse array of projects.</p>
+            <div className="job-info__tech">
+                <TechListing text="HTML"/>
+                <TechListing text="CSS"/>
+                <TechListing text="React"/>
+            </div>
         </div>
     );
 }
---
+++ src/components/TechListing.jsx
@@ -0,0 +1,9 @@
+import './TechListing.scss';
+
+const TechListing = (props) => {
+    return (
+        <div className="tech">{props.text}</div>
+    );
+}
+
+export default TechListing;
