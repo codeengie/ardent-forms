15598f404672ac16138b963c0f3a2c8705a0ac84
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 11:50:58 -0700

    Experience Module Class
    Updated the JobListing component to set a class.

--- src/modules/Experience.jsx
+++ src/modules/Experience.jsx
@@ -3,8 +3,8 @@ import JobListing from "../components/JobListing.jsx";
 const Experience = () => {
     return (
         <section className="experience">
-            <h2 className="experience__text">Experience</h2>
-            <JobListing/>
+            <h2 className="experience__title">Experience</h2>
+            <JobListing cName="experience__listing"/>
         </section>
     );
 }
