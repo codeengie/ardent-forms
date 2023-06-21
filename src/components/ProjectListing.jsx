44a85d541ecd100bb8b215694f5f41757aeb3f0f
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 17:34:14 -0700

    ProjectListing Component Image
    I swapped the image position and added a class name along with adding
    the TeckStack component.

--- src/components/ProjectListing.jsx
+++ src/components/ProjectListing.jsx
@@ -1,9 +1,13 @@
+import './ProjectListing.scss';
+import TechStack from "./TechStack.jsx";
+
 const ProjectListing = () => {
     return (
         <div className="project-listing">
+            <img className="project-listing__img" src="https://placehold.co/290x140" alt="Placeholder"/>
             <h3 className="project-listing__title">ShelfDat</h3>
             <p className="project-listing__detail">Risus ultricies tristique nulla aliquet enim tortor. Fermentum posuere urna nec tincidunt praesent semper feugiat. Non sodales neque sodales ut etiam sit amet nisl purus. Varius vel pharetra vel turpis. Nibh nisl condimentum id venenatis a condimentum vitae. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare.</p>
-            <img src="https://placehold.co/280x200" alt="Placeholder"/>
+            <TechStack/>
         </div>
     );
 }
