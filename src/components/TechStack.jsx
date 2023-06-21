088d9ff99bed3b8a828a56c17ae354abdd5d542b
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 17:36:34 -0700

    TechStack Title, List And Styles
    I updated the TechStack component to be a self contained component and
    added a wrapper, title, and list elements. I've also set defaultProps
    for the title.

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,11 +1,24 @@
-import './TechList.scss';
+import './TechStack.scss';
 
-const TechList = (props) => {
+const TechStack = (props) => {
     return (
-        <div className="tech-list">
-            
+        <div className="tech-stack">
+            <h4 className="tech-stack__title">{props.title}</h4>
+            <ul className="tech-stack__list">
+                <li className="tech-stack__list-item">HTML</li>
+                <li className="tech-stack__list-item">CSS</li>
+                <li className="tech-stack__list-item">JavaScript</li>
+                <li className="tech-stack__list-item">React</li>
+                <li className="tech-stack__list-item">Vite</li>
+                <li className="tech-stack__list-item">AWS</li>
+                <li className="tech-stack__list-item">Lambda</li>
+            </ul>
         </div>
     );
 }
 
-export default TechList;
+TechStack.defaultProps = {
+    title: 'Tech Stack'
+};
+
+export default TechStack;
