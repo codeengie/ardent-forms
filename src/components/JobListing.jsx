e4d9337132e5cd71ecb9aad0f2f1f95c7a171fe3
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 17:31:58 -0700

    JobListing Class Rename
    I renamed `job-info` to `job-listing` in the component and style file.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,20 +1,16 @@
 import './JobListing.scss';
-import TechListing from "./TechListing.jsx";
+import TechStack from "./TechStack.jsx";
 
 const JobListing = (props) => {
     return (
         // @todo Look into this linting error: "Missing in props validation"
-        <div className={`job-info ${props.cName}`}>
+        <div className={`job-listing ${props.cName}`}>
             <p>
-                <time dateTime="2015-08-01">August 2014</time> - <time dateTime="2022-03-26">March 2022</time>
+                <time dateTime="2015-08-01">Aug 2014</time> - <time dateTime="2022-03-26">Mar 2022</time>
             </p>
-            <h3 className="job-info__title">Senior Web Developer &middot; Stamps.com</h3>
-            <p className="job-info__text">Delivered high-quality, robust production code for a diverse array of projects.</p>
-            <div className="job-info__tech">
-                <TechListing text="HTML"/>
-                <TechListing text="CSS"/>
-                <TechListing text="React"/>
-            </div>
+            <h3 className="job-listing__title">Senior Web Developer &middot; Stamps.com</h3>
+            <p className="job-listing__text">Delivered high-quality, robust production code for a diverse array of projects. Sed egestas egestas fringilla phasellus. Nisi porta lorem mollis aliquam ut porttitor leo.</p>
+            <TechStack title="Tech"/>
         </div>
     );
 }
