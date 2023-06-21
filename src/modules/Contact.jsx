dba4448fcb4944f6ab115361b57df4cd74d63e91
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-20 17:38:31 -0700

    Contact, Form And Styles
    Created a basic form with some styles and updated `index.css` with a new
    color for input text and textarea.

--- src/modules/Contact.jsx
+++ src/modules/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './Contact.scss';
 
 const Contact = () => {
     const [movie, setMovie] = useState([]);
@@ -26,6 +27,13 @@ const Contact = () => {
         <section className="contact">
             <h2 className="contact__title">Contact</h2>
             <p className="contact__text">Feel free to contact me and I will get back to you as soon as possible</p>
+            <form className="contact__form">
+                <div className="input">
+                    <label className="input__label" htmlFor="label">Name</label>
+                    <input id="label" className="input__field" type="text"/>
+                </div>
+                <textarea className="textarea"></textarea>
+            </form>
             <button onClick={sendForm}>Send</button>
             <p>{movie.count}</p>
         </section>
