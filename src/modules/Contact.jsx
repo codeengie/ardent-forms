fe76fa7a2e164dbd0af9924e1715d4f0c96ae968
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-22 10:35:50 -0700

    Contact Form
    Created contact form with a bit of logic and styles. There's a bit of
    validation but I wont fully carve this out until I have the API wired
    and email service functional.

--- src/modules/Contact.jsx
+++ src/modules/Contact.jsx
@@ -2,40 +2,73 @@ import { useState } from 'react';
 import './Contact.scss';
 
 const Contact = () => {
-    const [movie, setMovie] = useState([]);
+    // Input fields
+    const [inputName, setInputName] = useState('');
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputNameIsValid, setInputNameIsValid] = useState(false);
+    const [inputNameTouched, setInputNameTouched] = useState(false);
 
-    // Functions need to be declared before return()
-    const sendForm = async () => {
-        console.log('Sent an email!');
+    const inputNameChangeHandler = event => {
+        setInputName(event.target.value);
+    }
 
-        try {
-            const response = await fetch('https://swapi.dev/api/films/');
+    const inputEmailChangeHandler = event => {
+        setInputEmail(event.target.value);
+    }
 
-            if (!response.ok) {
-                new Error(`Request failed with status ${response.status}`);
-            }
+    const formSubmitHandler = event => {
+        event.preventDefault();
 
-            const data = await response.json();
-            setMovie(data);
-            console.log(data);
-        } catch (error) {
-            console.error('Error', error.message);
+        setInputNameTouched(true);
+
+        if (inputName.trim() === '') {
+            setInputNameIsValid(false);
+            return;
         }
-    };
+
+        setInputNameIsValid(true);
+        setInputName('');
+        setInputEmail('')
+    }
+
+    const inputNameIsInvalid = !inputNameIsValid && inputNameTouched;
+
+    // Sets validation classes
+    const inputNameClass = inputNameIsInvalid ? 'contact__form-wrap contact__form-wrap--invalid' : 'contact__form-wrap'
 
     return (
         <section className="contact">
             <h2 className="contact__title">Contact</h2>
             <p className="contact__text">Feel free to contact me and I will get back to you as soon as possible</p>
-            <form className="contact__form">
-                <div className="input">
-                    <label className="input__label" htmlFor="label">Name</label>
-                    <input id="label" className="input__field" type="text"/>
+            <form className="contact__form" onSubmit={formSubmitHandler}>
+                <div className={inputNameClass}>
+                    {/*<label className="contact__form-label" htmlFor="name">Name</label>*/}
+                    <input
+                        className="contact__form-field"
+                        id="name"
+                        type="text"
+                        onChange={inputNameChangeHandler}
+                        placeholder="Name"
+                        value={inputName}
+                    />
+                    {inputNameIsInvalid && <p>Name must not be empty</p>}
                 </div>
-                <textarea className="textarea"></textarea>
+                <div className="contact__form-wrap">
+                    {/*<label className="contact__form-label" htmlFor="email">Email</label>*/}
+                    <input
+                        className="contact__form-field"
+                        id="email"
+                        type="email"
+                        onChange={inputEmailChangeHandler}
+                        placeholder="Email"
+                        value={inputEmail}
+                    />
+                </div>
+                <div className="contact__form-wrap contact__form-wrap--ta">
+                    <textarea className="contact__form-textarea" maxLength="415" placeholder="Message"></textarea>
+                </div>
+                <button className="contact__form-button">Send</button>
             </form>
-            <button onClick={sendForm}>Send</button>
-            <p>{movie.count}</p>
         </section>
     )
 }
