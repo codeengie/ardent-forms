c247f9b64c190084ecc125c43b67adbcbeebb022
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-22 18:21:36 -0700

    Module Subdirectories
    To continue to keep things organized I created subdirectories for all
    the modules that have a style file. Their import references were also
    updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.scss';
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import Intro from "./modules/Intro.jsx";
-import About from "./modules/About.jsx";
+import Intro from "./modules/Intro/Intro.jsx";
+import About from "./modules/About/About.jsx";
 import Experience from "./modules/Experience.jsx";
 import Projects from "./modules/Projects.jsx";
-import Contact from "./modules/Contact.jsx";
+import Contact from "./modules/Contact/Contact.jsx";
 
 const App = () => {
   return (
