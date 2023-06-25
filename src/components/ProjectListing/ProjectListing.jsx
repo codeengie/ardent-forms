fb86552930688d7a5b1ae43e8c428a18540365c4
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-25 12:04:02 -0700

    Source Code Availability
    Added a conditional to display text regarding the availability of my
    project's source code along with minor style changes and an update to
    the array.

--- src/components/ProjectListing/ProjectListing.jsx
+++ src/components/ProjectListing/ProjectListing.jsx
@@ -8,6 +8,12 @@ const ProjectListing = (props) => {
             <h3 className="project-listing__title">{props.projectData.title}</h3>
             <p className="project-listing__detail">{props.projectData.info}</p>
             <TechStack list={props.projectData.tech}/>
+            {props.projectData.code && (
+                <>
+                    <h4 className="project-listing__subtitle">Code</h4>
+                    <p className="project-listing__code">Source code is private but readily available to share with potential employers upon request.</p>
+                </>
+            )}
             <div className="project-listing__links">
                 <a className="project-listing__link" href={props.projectData.url} rel="noreferrer" target="_blank">{`View ${props.projectData.type}`}</a>
             </div>
--- src/modules/Projects.jsx
+++ src/modules/Projects.jsx
@@ -5,6 +5,7 @@ const Projects = () => {
     // @todo Refactor to use API
     const projects = [
         {
+            code: true,
             date: '2022-06-15',
             image: 'https://placehold.co/290x140',
             info: 'Amet aliquam id diam maecenas. Lectus proin nibh nisl condimentum id venenatis. Magnis dis parturient montes nascetur ridiculus mus. Duis tristique sollicitudin nibh sit.',
@@ -14,6 +15,7 @@ const Projects = () => {
             url: 'https://ardentforms.com'
         },
         {
+            code: true,
             date: '2022-03-05',
             image: 'https://placehold.co/290x140',
             info: 'Justo laoreet sit amet cursus sit amet dictum sit. Et malesuada fames ac turpis egestas integer. Ante metus dictum at tempor commodo ullamcorper a lacus.',
@@ -23,6 +25,7 @@ const Projects = () => {
             url: 'https://thecavedev.com'
         },
         {
+            code: true,
             date: '2021-02-15',
             image: 'https://placehold.co/290x140',
             info: 'Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. In iaculis nunc sed augue lacus viverra. Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus. Lacus viverra vitae congue eu consequat ac.',
@@ -32,6 +35,7 @@ const Projects = () => {
             url: 'https://shelfdat.com'
         },
         {
+            code: false,
             date: '2020-09-11',
             image: 'https://placehold.co/290x140',
             info: 'Ornare lectus sit amet est placerat in egestas erat. Cras sed felis eget velit aliquet sagittis. Dolor sit amet consectetur adipiscing. Augue interdum velit euismod in pellentesque massa placerat duis.',
@@ -41,6 +45,7 @@ const Projects = () => {
             url: 'https://endicia.com'
         },
         {
+            code: false,
             date: '2017-06-11',
             image: 'https://placehold.co/290x140',
             info: 'Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Pulvinar elementum integer enim neque volutpat.',
@@ -50,6 +55,7 @@ const Projects = () => {
             url: 'https://shipworks.com'
         },
         {
+            code: false,
             date: '2016-04-22',
             image: 'https://placehold.co/290x140',
             info: 'Scelerisque in dictum non consectetur. Sed id semper risus in hendrerit gravida rutrum quisque non. Libero nunc consequat interdum varius sit amet mattis.',
@@ -59,6 +65,7 @@ const Projects = () => {
             url: 'https://webregistration.com'
         },
         {
+            code: false,
             date: '2015-12-23',
             image: 'https://placehold.co/290x140',
             info: 'Libero id faucibus nisl tincidunt eget nullam non nisi est. Ultrices vitae auctor eu augue ut lectus arcu. Turpis egestas maecenas pharetra convallis posuere morbi leo urna.',
