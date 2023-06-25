dc3b627761e5f1b0ae233cd9d33f606fb590f742
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-25 11:40:33 -0700

    Projects Rendering w/Data
    Created data in Projects component to mock the data I will be
    pulling from database and updated component to iterate this data. I
    also updated the ProjeListing component to use props and pass the data
    into the different parts of the component.

--- src/components/ProjectListing/ProjectListing.jsx
+++ src/components/ProjectListing/ProjectListing.jsx
@@ -1,13 +1,16 @@
 import './ProjectListing.scss';
 import TechStack from "../TechStack/TechStack.jsx";
 
-const ProjectListing = () => {
+const ProjectListing = (props) => {
     return (
         <div className="project-listing">
             <img className="project-listing__img" src="https://placehold.co/290x140" alt="Placeholder"/>
-            <h3 className="project-listing__title">ShelfDat</h3>
-            <p className="project-listing__detail">Risus ultricies tristique nulla aliquet enim tortor. Fermentum posuere urna nec tincidunt praesent semper feugiat. Non sodales neque sodales ut etiam sit amet nisl purus. Varius vel pharetra vel turpis. Nibh nisl condimentum id venenatis a condimentum vitae. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare.</p>
-            <TechStack/>
+            <h3 className="project-listing__title">{props.projectData.title}</h3>
+            <p className="project-listing__detail">{props.projectData.info}</p>
+            <TechStack list={props.projectData.tech}/>
+            <div className="project-listing__links">
+                <a className="project-listing__link" href={props.projectData.url} rel="noreferrer" target="_blank">{`View ${props.projectData.type}`}</a>
+            </div>
         </div>
     );
 }
--- src/modules/Projects.jsx
+++ src/modules/Projects.jsx
@@ -1,10 +1,78 @@
 import ProjectListing from "../components/ProjectListing/ProjectListing.jsx";
 
 const Projects = () => {
+    // Temporary data while I implement API and DB
+    // @todo Refactor to use API
+    const projects = [
+        {
+            date: '2022-06-15',
+            image: 'https://placehold.co/290x140',
+            info: 'Amet aliquam id diam maecenas. Lectus proin nibh nisl condimentum id venenatis. Magnis dis parturient montes nascetur ridiculus mus. Duis tristique sollicitudin nibh sit.',
+            tech: ['React', 'Vite', 'HTML', 'SCSS', 'AWS'],
+            title: 'Ardent Forms',
+            type: 'App',
+            url: 'https://ardentforms.com'
+        },
+        {
+            date: '2022-03-05',
+            image: 'https://placehold.co/290x140',
+            info: 'Justo laoreet sit amet cursus sit amet dictum sit. Et malesuada fames ac turpis egestas integer. Ante metus dictum at tempor commodo ullamcorper a lacus.',
+            tech: ['Vue.js', 'HTML', 'SCSS', 'AWS'],
+            title: 'The Cave Dev',
+            type: 'App',
+            url: 'https://thecavedev.com'
+        },
+        {
+            date: '2021-02-15',
+            image: 'https://placehold.co/290x140',
+            info: 'Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. In iaculis nunc sed augue lacus viverra. Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus. Lacus viverra vitae congue eu consequat ac.',
+            tech: ['Vue.js', 'HTML', 'SCSS', 'JavaScript', 'AWS'],
+            title: 'ShelfDat',
+            type: 'App',
+            url: 'https://shelfdat.com'
+        },
+        {
+            date: '2020-09-11',
+            image: 'https://placehold.co/290x140',
+            info: 'Ornare lectus sit amet est placerat in egestas erat. Cras sed felis eget velit aliquet sagittis. Dolor sit amet consectetur adipiscing. Augue interdum velit euismod in pellentesque massa placerat duis.',
+            tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
+            title: 'Endicia',
+            type: 'Website',
+            url: 'https://endicia.com'
+        },
+        {
+            date: '2017-06-11',
+            image: 'https://placehold.co/290x140',
+            info: 'Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Pulvinar elementum integer enim neque volutpat.',
+            tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
+            title: 'Shipworks',
+            type: 'Website',
+            url: 'https://shipworks.com'
+        },
+        {
+            date: '2016-04-22',
+            image: 'https://placehold.co/290x140',
+            info: 'Scelerisque in dictum non consectetur. Sed id semper risus in hendrerit gravida rutrum quisque non. Libero nunc consequat interdum varius sit amet mattis.',
+            tech: ['Steal.js', '.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
+            title: 'Web Registration',
+            type: 'App',
+            url: 'https://webregistration.com'
+        },
+        {
+            date: '2015-12-23',
+            image: 'https://placehold.co/290x140',
+            info: 'Libero id faucibus nisl tincidunt eget nullam non nisi est. Ultrices vitae auctor eu augue ut lectus arcu. Turpis egestas maecenas pharetra convallis posuere morbi leo urna.',
+            tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
+            title: 'Stamps',
+            type: 'Website',
+            url: 'https://stamps.com'
+        }
+    ];
+
     return (
         <section className="projects">
             <h2 className="projects__title">Projects</h2>
-            <ProjectListing/>
+            {projects.map((project, index) => <ProjectListing key={index} projectData={project}/>)}
         </section>
     );
 }
