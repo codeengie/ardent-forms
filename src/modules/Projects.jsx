d1a8b512f209f1a72efcf6e0041ceffe68721cbc
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-29 09:17:39 -0700

    Project Data
    Updated projects data with actual content. This will eventually be
    coming from AWS.

--- src/modules/Projects.jsx
+++ src/modules/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
             date: '2022-06-15',
             demo: false,
             image: {
-                alt: 'Placeholder Image',
+                alt: 'A screenshot of Ardent Forms web application showing a few of the sections with content',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/ardent-forms.webp'
             },
             info: 'Ardent Forms is my portfolio designed to showcase the websites and applications I\'ve developed in the past and present. Its built using a modern web stack and boasts a responsive design, ensuring a seamless and optimized experience across desktop, tablet, and mobile devices.',
@@ -40,7 +40,7 @@ const Projects = () => {
                 alt: 'A screenshot of the ShelfDat web application showing a dashboard interface with recent media',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/shelfdat.webp'
             },
-            info: 'ShelfDat is an inventory management web application. It enables the user to easily track what they have and where it is at anytime on any device. ShelfDat is built using a modern web stack and boasts a responsive design, ensuring a seamless and optimized experience across desktop, tablet, and mobile devices.',
+            info: 'ShelfDat is an inventory management web application. It enables the user to easily track what they have and where it is at anytime on any device. Its built using a modern web stack and boasts a responsive design, ensuring a seamless and optimized experience across desktop, tablet, and mobile devices.',
             tech: ['Vue.js', 'HTML', 'SCSS', 'JavaScript', 'Node.js', 'NPM', 'AWS'],
             title: 'ShelfDat',
             type: 'App',
@@ -51,10 +51,10 @@ const Projects = () => {
             date: '2020-09-11',
             demo: false,
             image: {
-                alt: 'Placeholder Image',
+                alt: 'A screenshot of Endicia website homepage with a large hero banner colored in cyan',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/endicia.webp'
             },
-            info: 'Ornare lectus sit amet est placerat in egestas erat. Cras sed felis eget velit aliquet sagittis. Dolor sit amet consectetur adipiscing. Augue interdum velit euismod in pellentesque massa placerat duis.',
+            info: 'Endicia is a leading provider for online shipping solutions that empower businesses and individuals to streamline their shipping processes and maximize efficiency.',
             tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
             title: 'Endicia',
             type: 'Website',
@@ -65,10 +65,10 @@ const Projects = () => {
             date: '2017-06-11',
             demo: false,
             image: {
-                alt: 'Placeholder Image',
+                alt: 'A screenshot of Shipworks website homepage with a big heading and a video with a orange button',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/shipworks.webp'
             },
-            info: 'Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Pulvinar elementum integer enim neque volutpat.',
+            info: 'ShipWorks is a cutting-edge e-commerce shipping and order management platform designed to simplify and streamline the shipping process for online businesses of all sizes.',
             tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
             title: 'Shipworks',
             type: 'Website',
@@ -79,12 +79,12 @@ const Projects = () => {
             date: '2016-04-22',
             demo: false,
             image: {
-                alt: 'Placeholder Image',
+                alt: 'A screenshot of Stamps Registration showing a sign up form with a step tracker on top',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/webreg.webp'
             },
-            info: 'Scelerisque in dictum non consectetur. Sed id semper risus in hendrerit gravida rutrum quisque non. Libero nunc consequat interdum varius sit amet mattis.',
-            tech: ['Steal.js', '.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
-            title: 'Web Registration',
+            info: 'Stamps Registration is the convenient and streamlined platform designed to help you seamlessly sign up for Stamps online postage service.',
+            tech: ['React', '.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript'],
+            title: 'Stamps Registration',
             type: 'App',
             url: 'https://registration.stamps.com'
         },
@@ -93,10 +93,10 @@ const Projects = () => {
             date: '2015-12-23',
             demo: false,
             image: {
-                alt: 'Placeholder Image',
+                alt: 'A screenshot of Stamps website homepage with an image of cartoon characters and a mailbox',
                 source: 'https://s3.us-west-1.amazonaws.com/images.ardentforms.com/images/projects/stamps.webp'
             },
-            info: 'The design is clean, modern, and visually appealing, allowing users to navigate effortlessly through the various sections and access the information they need. With its responsive layout, the Stamps.com website ensures a consistent and optimized experience across desktop and mobile devices',
+            info: 'Stamps is an innovative and user-friendly online postage service that revolutionizes the way individuals and businesses handle their mailing needs.',
             tech: ['.NET', 'SQL', 'HTML', 'SCSS', 'Bootstrap', 'JavaScript', 'jQuery'],
             title: 'Stamps',
             type: 'Website',
