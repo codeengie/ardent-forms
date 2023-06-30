fa47fce72d5d6be7385484e4d195f97880e617d1
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-30 13:20:34 -0700

    Backdrop Component And No Background Scrolling
    Created a backdrop component and moved corresponding styles I created
    in the Nav component. I originally was using portals to add the
    backdrop at the root of index.html but removed it because it was not as
    flexible with z-layers.
    
    Secondly, created a close button and added it to the nav component with a
    few styles.
    
    Finally, updated the `menu-context` file and added a toggle function to
    add a `noscroll` class to the body so when the menu is opened the user
    will not be able to scroll the background. You see this in a lot of
    websites and I personally don't like it.

---
+++ src/components/Backdrop/Backdrop.jsx
@@ -0,0 +1,14 @@
+import './Backdrop.scss';
+import { useContext } from 'react';
+import MenuContext from '../../../store/menu-context.jsx';
+
+const Backdrop = () => {
+    const ctxBackdrop = useContext(MenuContext);
+    const toggleClass = ctxBackdrop.isMenuOpen ? 'backdrop backdrop--show' : 'backdrop';
+
+    return (
+        <div className={toggleClass} onClick={ctxBackdrop.onMenuClick}></div>
+    );
+};
+
+export default Backdrop;
--- store/menu-context.jsx
+++ store/menu-context.jsx
@@ -14,6 +14,7 @@ export const MenuContextProvider = (props) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleMenuState = () => {
+        document.body.classList.toggle('noscroll');
         setIsMenuOpen(!isMenuOpen);
     }
 
