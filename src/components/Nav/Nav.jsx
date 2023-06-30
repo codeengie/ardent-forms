21eaa7c13577864263a50befbf654fd58a37530b
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-30 15:04:46 -0700

    Disable Menu Click On Resize
    Updated the Nav component to use ResizeObserver interface so I can set
    a conditional as to when a button click can be invoked. I also removed
    remnants of the Backdrop component.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,27 +1,45 @@
-import { useContext } from "react";
-import ReactDOM from "react-dom";
+import { useContext, useEffect, useRef, useState } from 'react';
 import './Nav.scss';
-import MenuContext from "../../../store/menu-context.jsx";
-
-const Backdrop = () => {
-    const ctxBackdrop = useContext(MenuContext);
-    const toggleClass = ctxBackdrop.isMenuOpen ? 'backdrop backdrop--show' : 'backdrop';
-    return <div className={toggleClass} onClick={ctxBackdrop.onMenuClick}></div>
-}
+import MenuContext from '../../../store/menu-context.jsx';
 
 const Nav = () => {
     const ctxMenu = useContext(MenuContext);
     const toggleClass = ctxMenu.isMenuOpen ? 'nav nav--open' : 'nav';
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+    const resizeObserverRef = useRef(null)
+
+    /*
+     * Tapping into the ResizeObserver interface in order to keep track of the dimensional
+     * changes of the viewport. This was necessary to help create a conditional so `handleClick()`
+     * only fires on a mobile device.
+     */
+    useEffect(() => {
+        resizeObserverRef.current = new ResizeObserver((entries) => {
+            for (let entry of entries) {
+                setViewportWidth(entry.contentRect.width);
+            }
+        });
+
+        // Set target element to observe
+        resizeObserverRef.current.observe(document.documentElement);
+
+        // This has to return anonymous function otherwise viewport width will not update
+        return () => {
+            resizeObserverRef.current.disconnect();
+        }
+
+    }, []);
 
     const handleLinkClick = () => {
-        ctxMenu.onMenuClick();
+        if (viewportWidth <= 767) {
+            ctxMenu.onMenuClick();
+        }
     };
 
     return (
         <>
-            {ReactDOM.createPortal(<Backdrop/>, document.getElementById('backdrop-root'))}
             <nav className={toggleClass}>
-                <div className="nav__backdrop"></div>
+                <button className="nav__close" type="button" aria-label="Close" onClick={handleLinkClick}></button>
                 <ul className="nav__list">
                     <li className="nav__list-item">
                         <a className="nav__list-link" href="#aboutRef" onClick={handleLinkClick}>About</a>
