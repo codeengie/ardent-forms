f6ec18b7e7f9d0defd4de12674ec4a9fd1515889
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-06-30 13:24:18 -0700

    Menu Context Removal From MenuToggle
    I removed the MenuContext from the MenuToggle component as it will no
    longer serve as a close button since I added a button in the menu
    component.

--- src/components/MenuToggle/MenuToggle.jsx
+++ src/components/MenuToggle/MenuToggle.jsx
@@ -1,13 +1,12 @@
 import { useContext } from 'react';
 import './MenuToggle.scss';
-import MenuContext from "../../../store/menu-context.jsx";
+import MenuContext from '../../../store/menu-context.jsx';
 
 const MenuToggle = () => {
     const ctx = useContext(MenuContext);
-    const toggleClass = ctx.isMenuOpen ? 'menu-toggle menu-toggle--open' : 'menu-toggle'
 
     return (
-        <div className={toggleClass} onClick={ctx.onMenuClick}>
+        <div className="menu-toggle" onClick={ctx.onMenuClick}>
             <div className="menu-toggle__menu"></div>
         </div>
     );
