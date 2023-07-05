cb8c0c63987fc972af6939c338a05991fd5a0610
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-07-05 11:27:30 -0700

    PSI Responsive Images
    In the quest to get the best score from PageSpeed Insights I created
    multiple images geared for viewports. Built-in lazy loading attribute
    was also added.

--- src/components/ProjectListing/ProjectListing.jsx
+++ src/components/ProjectListing/ProjectListing.jsx
@@ -2,14 +2,28 @@ import './ProjectListing.scss';
 import TechStack from "../TechStack/TechStack.jsx";
 
 const ProjectListing = (props) => {
+    // Remove `.webp` from image data @todo Fix this in the DB
+    const imageName = props.data.Image.Source.replace(/\.webp/g, '');
+    const viewports = [576, 768, 992, 1200];
+    const imageSources = viewports
+        .map(view => `${import.meta.env.VITE_IMG_URL}${imageName}-${view}w.webp ${view}w`)
+        .toString();
+
     return (
         <div className="project-listing">
             <img
+                alt={props.data.Image.Alt}
                 className="project-listing__img"
                 height="204"
-                src={props.data.Image.Source}
+                loading="lazy"
+                srcSet={imageSources}
+                sizes="
+                    (max-width: 576px) 576w,
+                    (max-width: 768px) 768w,
+                    (max-width: 992px) 992w,
+                    (max-width: 1200px) 1200w"
+                src={`${import.meta.env.VITE_IMG_URL}${imageName}-1200w.webp`}
                 width="280"
-                alt={props.data.Image.Alt}
             />
             <h3 className="project-listing__title">{props.data.Title}</h3>
             <p className="project-listing__detail">{props.data.Info}</p>
