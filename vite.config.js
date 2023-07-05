4054b14076823112a339aba071ba67d26b71ded1
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-07-05 07:48:28 -0700

    Vite Config
    Made a few quality of life changes to `vite.config.js` file. With the
    use of Vite's basicSSL package and configuring API Gateway to enable
    CORS I am able to pull in data from the server.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://vitejs.dev/config/
+/**
+ * Review "Configuring Vite" page if you need to add conditional
+ * changes to configuration.
+ * @link https://vitejs.dev/config/
+ */
 export default defineConfig({
+  server: {
+    host: 'ardentforms.com',
+    open: 'browser',
+    port: 443
+  },
   plugins: [react(), basicSsl()],
 })
