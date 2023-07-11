ea2f38cce9e53591774f38af8502f7b8eaade6b2
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-07-11 13:05:01 -0700

    Vite-Plugin-Html
    Added `vite-plugin-html` to project. This will allow me to dynamically
    modify `index.html` depending on environment. This is still a work in
    progress.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 /**
  * Review "Configuring Vite" page if you need to add conditional
  * changes to configuration.
  * @link https://vitejs.dev/config/
+ * @link https://vitejs.dev/guide/env-and-mode.html
+ * @todo Create a custom config for prod
  */
 export default defineConfig({
   server: {
@@ -13,5 +16,16 @@ export default defineConfig({
     open: 'browser',
     port: 443
   },
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          bots: `<meta name="robots" content="noindex,nofollow">`,
+        }
+      }
+    })
+  ],
 })
