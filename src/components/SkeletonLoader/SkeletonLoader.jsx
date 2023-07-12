c20748e7532941110d1d8c673de1612c0ed6525d
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-07-12 10:36:09 -0700

    Skeleton Loader Component
    Tweaked the skeleton loader with final color scheme and removed
    remnants of the old one. I also changed the animation from fade to
    a transform translateX.

--- src/components/SkeletonLoader/SkeletonLoader.jsx
+++ src/components/SkeletonLoader/SkeletonLoader.jsx
@@ -8,15 +8,16 @@ const SkeletonLoader = (props) => {
     for (let i = 0; i < props.count; i++) {
         skeletons.push(
             <div className={modifier} key={i}>
-                <p className="skeleton__time"></p>
-                <h3 className="skeleton__title"></h3>
-                <p className="skeleton__body"></p>
-                <h4 className="skeleton__subtitle"></h4>
-                <ul className="skeleton__list">
-                    <li className="skeleton__list-item"></li>
-                    <li className="skeleton__list-item"></li>
-                    <li className="skeleton__list-item"></li>
-                    <li className="skeleton__list-item"></li>
+                <span className="skeleton__hidden" aria-hidden="false">Loading... Please wait.</span>
+                <p className="skeleton__time" aria-hidden="true"></p>
+                <h3 className="skeleton__title" aria-hidden="true"></h3>
+                <p className="skeleton__body" aria-hidden="true"></p>
+                <h4 className="skeleton__subtitle" aria-hidden="true"></h4>
+                <ul className="skeleton__list" aria-hidden="true">
+                    <li className="skeleton__list-item" aria-hidden="true"></li>
+                    <li className="skeleton__list-item" aria-hidden="true"></li>
+                    <li className="skeleton__list-item" aria-hidden="true"></li>
+                    <li className="skeleton__list-item" aria-hidden="true"></li>
                 </ul>
             </div>
         );
