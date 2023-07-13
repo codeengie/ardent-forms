18673ffd25622f3f8d7af87c42201e8c036b72ef
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-07-12 21:01:12 -0700

    Dev/Prod Index File Build Config
    I was able to generate different `index.html` files but it keeps the name
    of the source file. I'll need to figure out how to rename it while it
    builds.

--- vite.config.js
+++ vite.config.js
@@ -10,22 +10,45 @@ import { createHtmlPlugin } from 'vite-plugin-html';
  * @link https://vitejs.dev/guide/env-and-mode.html
  * @todo Create a custom config for prod
  */
-export default defineConfig({
+
+// Build config
+let params = {
+  filename: 'index.html',
+  template: 'src/html/index.dev.html',
+  inject: {
+    data: {
+      title: 'Ardent Forms',
+      desc: 'Ardent Forms is where I display my past and present web development projects.'
+    }
+  }
+};
+
+// Vite config
+let options = {
   server: {
     host: 'ardentforms.com',
     open: 'browser',
     port: 443
   },
   plugins: [
-    react(),
-    basicSsl(),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        data: {
-          bots: `<meta name="robots" content="noindex,nofollow">`,
-        }
-      }
-    })
-  ],
-})
+      react(),
+      basicSsl(),
+      createHtmlPlugin(params)
+  ]
+};
+
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'production') {
+    params.filename = 'dist/index.html';
+    params.template = 'src/html/index.prod.html';
+    params.minify = true;
+    params.inject.data.bots = `<meta name="robots" content="noindex,nofollow">`;
+    params.inject.data.gtag = `
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0GHPHTM29V"></script>
+        <script>window.dataLayer = window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-0GHPHTM29V');</script>`;
+  }
+
+  console.log(params);
+
+  return options;
+});
