235b2c128c7ef0ffb839feb22e4a5370c7224a56
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-10-24 22:01:45 -0700

    About
    Updated about component with a revised about me text.

--- src/modules/About/About.jsx
+++ src/modules/About/About.jsx
@@ -3,8 +3,6 @@ import './About.scss';
 import ScrollContext from '../../store/scroll-context.jsx';
 
 const About = () => {
-    const gradYear = 2010;
-    const yearsOfExperience = new Date().getFullYear() - gradYear;
     const ctx = useContext(ScrollContext);
     const aboutRef = useRef();
 
@@ -17,9 +15,10 @@ const About = () => {
     return (
         <section className="about" ref={aboutRef}>
             <h2 className="about__title">About</h2>
-            <p className="about__text">I began my coding journey {yearsOfExperience} years ago immediately after earning my bachelor&rsquo;s in Interactive Media and Web Design from The Art Institute. Who would&rsquo;ve thought all the years I spent training and honing my design eye would lead to development?</p>
-            <p className="about__text">These days I transform intricate designs into well-crafted, stunning and beautiful responsive websites that are visually flawless down to the pixel.</p>
-            <p className="about__text">On the days I&rsquo;m not buried in my IDE, you&rsquo;ll find me indulging in PC games, 3D modeling and printing, or creating digital art on my tablet.</p>
+            <p className="about__text">I embarked on my coding journey over a decade ago, right after completing my bachelor&rsquo;s degree in Interactive Media and Web Design at The Art Institute. Little did I know that my years of dedicated training and refining my design sensibilities would ultimately lead me into the world of web development.</p>
+            <p className="about__text">Today, I specialize in translating complex designs into meticulously crafted, visually stunning, and flawlessly responsive websites, ensuring that every pixel is perfectly aligned for a seamless user experience.</p>
+            <p className="about__text">When I&rsquo;m not immersed in my Integrated Development Environment (IDE), you&rsquo;ll often find me immersed in other creative pursuits. I enjoy indulging in PC gaming, exploring 3D modeling and printing, and expressing my creativity through digital art on my tablet.</p>
+            <p className="about__text"></p>
         </section>
     );
 }
